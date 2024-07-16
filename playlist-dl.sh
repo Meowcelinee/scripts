df925fd6738b4fd84f1ec633110acc8b6e7f5f36
Natalie Raine <nyatalieeee@proton.me>
2024-07-16 00:43:35 -0500

    added errors for no input

--- playlist-dl.sh
+++ playlist-dl.sh
@@ -18,18 +18,29 @@ clear
 echo -e "yt-dlp version $version"
 echo -e "script written by Natalie Raine :3\n"
 
-echo -e "${bold}Paste the playlist URL:${normal}"
-read URL
+echo -e "${bold}Paste the playlist URL (ctrl+shift+V):${normal}"
+read URL && sleep 0.05
 
-echo -e "\n${bold}Enter the directory to download the songs to:\n(example: 'music/playlistname')\n${YELLOW}(NOTE: Path must be relative to your home direcotry)${TEXT}${normal}"
-read DIR_NAME
+if [ ! $URL ]
+then
+    echo -e "${RED}${bold}ERROR:${TEXT} You need to input a playlist URL.${normal}"
+    exit 1
+fi
+
+echo -e "\n\n${bold}Enter the directory to download the contents to:"
+echo -e "${YELLOW}NOTE: Path must be relative to your home directory.\nExample: 'Music/playlist' INSTEAD OF '/home/${USER}/Music/playlist'${TEXT}${normal}"
+read DIR_NAME && sleep 0.05
+
+if [ ! $DIR_NAME ]
+then
+    echo -e "${RED}${bold}ERROR:${TEXT} You need to input a download directory.${normal}"
+    exit 1
+fi
 
 cd $HOME
 echo -e "${GREEN}${bold}"
 mkdir -pv $DIR_NAME && cd $DIR_NAME
 echo -e "${TEXT}${normal}"
-
-sleep 0.8
 
 echo -ne "${bold}Beginning download.\r" && sleep 1
 echo -ne "Beginning download..\r" && sleep 1
@@ -40,4 +51,4 @@ yt-dlp --yes-playlist --newline --extract-audio --restrict-filenames --audio-for
 sleep 0.4
 
 echo -e "\n${GREEN}${bold}Download complete!"
-echo -e "Songs can be found in ${HOME}/${DIR_NAME}${TEXT}${normal}\n"
+echo -e "Contents can be found in ${HOME}/${DIR_NAME}${TEXT}${normal}\n"
