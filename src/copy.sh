bc34d9df774702aeaf40a9968e2645cf603a3978
Natalie Raine <nyatalieeee@proton.me>
2024-07-19 15:15:24 -0500

    added script to symlink scripts to ~/.local/bin

---
+++ src/copy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIR=$(pwd)
+GREEN="\033[32m"
+TEXT="\033[0;39m"
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo ""
+
+for i in $DIR/*
+do
+    sleep 0.15
+    ln -sfv $i ~/.local/bin/
+done
+
+echo -e "\n${GREEN}${bold}Scripts successfully copied!${TEXT}${normal}\n"
